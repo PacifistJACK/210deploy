c77ea99125e47a570346e2eeb290952c042ff2fc
Utkarsh Sharma <88643337+PacifistJACK@users.noreply.github.com>
2025-03-19 00:10:39 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, text
 
 engine = create_engine(
-    "mysql+pymysql://freedb_jadu1:yu!@hrsBgSx9JZa@sql.freedb.tech:3306/freedb_jadukeproject1?charset=utf8mb4"
+    "mysql+pymysql://freedb_jadu1:yu%21%40hrsBgSx9JZa@sql.freedb.tech:3306/freedb_jadukeproject1?charset=utf8mb4"
 )
