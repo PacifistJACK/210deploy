da11231329703304f8967d1dd7b7d31ea38680ec
Utkarsh Sharma <utkarshsharma3600@gmail.com>
2025-03-18 23:45:36 +0530

    changes

--- database.py
+++ database.py
@@ -1,20 +1,5 @@
 from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
-import os
 
-# Load environment variables from the .env file
-load_dotenv()
-
-# Get the database details from environment variables
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-# Create the engine securely using environment variables
 engine = create_engine(
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
+    "mysql+pymysql://freedb_jadu1:yu%21%40hrsBgSx9JZa@sql.freedb.tech:3306/freedb_jadukeproject1?charset=utf8mb4"
 )
-
-
