4fe97459a5ae994e45156b541c54e8f395608a61
Utkarsh Sharma <88643337+PacifistJACK@users.noreply.github.com>
2025-03-21 08:13:38 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, text
 
 # Database connection setup
 engine = create_engine(
-    "mysql+pymysql://freedb_jadu1:yu%21%40hrsBgSx9JZa@sql.freedb.tech:3306/freedb_jadukeproject1?charset=utf8mb4"
 )
 
 # Function to load all jobs into a list of dictionaries
